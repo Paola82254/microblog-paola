006db0f06ce0d4589ff8dab193a8f31cecf1c900
Paola82254 <teacherpaola4kids@gmail.com>
2023-11-22 15:16:37 -0300

    update-22-11

--- inc/funcoes-sessao.php
+++ inc/funcoes-sessao.php
@@ -26,3 +26,24 @@ function verificaAcesso(){
         exit; // ou die()
     }
 }
+
+
+
+function login($id, $nome, $tipo){
+    /* Criação de variáveis de sessão
+    Recursos que ficam disponíveis para o uso
+    durante toda a duração da sessão, ou seja, enquanto o navegador não for fechado
+    ou o usuário não clicar em sair
+     */
+    $_SESSION["id"] = $id;
+    $_SESSION["nome"] = $nome;
+    $_SESSION["tipo"] = $tipo;
+}
+
+function logout(){
+    session_destroy();
+    header("location:../login.php?saiu");
+    exit; // ou die()
+
+
+}
